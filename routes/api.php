5996e7cf8aae18c0b7c95e1c6f501cf96b43120d
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-09-09 13:10:23 +0200

    Chore: Apii functiionalities

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/clientAll',function (){
+    return Client::all();
+});
+
+Route::post('/createClient',function (){
+    return Client::create([
+        
+    ]);
 });
