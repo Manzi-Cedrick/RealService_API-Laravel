8dafb6422d8970083192d808f10018bd233c3e0f
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-09-12 13:49:58 +0200

    Migration Table

---
+++ database/migrations/2022_09_11_214207_create_stocks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stocks', function (Blueprint $table) {
+            $table->id();
+            $table->name('Stock_Name');
+            $table->integer('Stock_Quantity');
+            $table->date('Registration_Date');
+            $table->date('Expiration_Date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stocks');
+    }
+};
