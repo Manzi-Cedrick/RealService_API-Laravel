332177aecd6fee2ab367904c7a629bd21aefc5ec
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-09-22 15:33:40 +0200

    Auth methods and testing

--- app/Http/Controllers/Auth.php
+++ app/Http/Controllers/Auth.php
@@ -2,34 +2,63 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth as FacadesAuth;
 use Illuminate\Support\Facades\Hash;
+use Laravel\Sanctum\PersonalAccessToken;
 
 class Auth extends Controller
 {
     //
     public function Register(Request $request){
-        $RegisteredUser = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password)
+        $fields = $request->validate([
+            'name'=>'required|string',
+            'email' =>['required','email','unique:users'],
+            'password'=> ['required','min:6','confirmed']
         ]);
+        $RegisteredUser = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'password' => Hash::make($fields['password'])
+        ]);
+        $token = $RegisteredUser->createToken('AppToken')->plainTextToken;
         return response()->json([
             'status' => true,
             'message' => 'User successfully Registered',
             'User' => $RegisteredUser,
+            'Token' => $token
         ],200);
     }
     public function Login(Request $request){
-        if(FacadesAuth::attempt($request->only('email','password'))){
+        $fields = $request->validate([
+            'email' =>['required','email'],
+            'password'=> ['required','min:6']
+        ]);
+        $user = User::where('email',$fields['email'])->first();
+        if(!$user || !Hash::check($fields['password'],$user->password)){
             return response()->json([
                 'status' => true,
-                'message' => 'User successfully Login Success',
-                'User' => FacadesAuth::user()
+                'message' => 'Bad Credentials',
+            ],401);
+        }
+            $token = $user->createToken('AppToken')->plainTextToken;
+            return response()->json([
+                'status' => true,
+                'message' => 'User Successfully Logged In',
+                'User' => $user,
+                'Token' => $token
+            ],200);
+        
+    }
+    public function Logout(Request $request){
+        $accessToken = $request->bearerToken();
+        $token = PersonalAccessToken::findToken($accessToken);
+        if($token->delete()){
+        return response()->json([
+                'status' => true,
+                'message' => 'User successfully Logged Out',
             ],200);
         }
     }
