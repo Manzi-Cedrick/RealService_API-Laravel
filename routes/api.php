b48120c610efbfec85fb5a705f79d30317381214
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-09-22 15:46:01 +0200

    Protected Routes Checking and Testing

--- routes/api.php
+++ routes/api.php
@@ -26,28 +26,34 @@ Route::post('/register',[Auth::class,'Register']);
 Route::post('/login',[Auth::class,'Login']);
 Route::post('/logout',[Auth::class,'Logout']);
 
-Route::get('/clientAll',[ClientControl::class,'index'])->middleware('auth:sanctum');
-Route::post('/createClient',[ClientControl::class,'store']);
-Route::put('/updateClient/{client}',[ClientControl::class,'update']);
-Route::delete('/deleteClient/{client}',[ClientControl::class,'destroy']);
-Route::get('/singleClientInfo/{client}',[ClientControl::class,'ShowSingleClient']);
-Route::get('/allsales',[ClientControl::class,'AllSales']);
-Route::get('/clientsearch',[ClientControl::class,'SearchList']);
+//Protected Client Routes
+Route::group(['middleware'=>'auth:sanctum'],function (){
+    Route::get('/clientAll',[ClientControl::class,'index']);
+    Route::post('/createClient',[ClientControl::class,'store']);
+    Route::put('/updateClient/{client}',[ClientControl::class,'update']);
+    Route::delete('/deleteClient/{client}',[ClientControl::class,'destroy']);
+    Route::get('/singleClientInfo/{client}',[ClientControl::class,'ShowSingleClient']);
+    Route::get('/allsales',[ClientControl::class,'AllSales']);
+    Route::get('/clientsearch',[ClientControl::class,'SearchList']);
+});
 
-//Product routes
+//Protected Product routes
+Route::group(['middleware' => 'auth:sanctum'],function (){
+    Route::get('/productsAll',[ProductController::class,'index']);
+    Route::post('/createproduct',[ProductController::class,'store']);
+    Route::put('/updateproduct/{product}',[ProductController::class,'update']);
+    Route::delete('/deleteproduct/{product}',[ProductController::class,'destroy']);
+    Route::get('/singleproductClients/{product}',[ProductController::class,'ShowSingleProductClients']);
+    Route::get('/productsearch',[ProductController::class,'SearchList']);
+});
 
-Route::get('/productsAll',[ProductController::class,'index']);
-Route::post('/createproduct',[ProductController::class,'store']);
-Route::put('/updateproduct/{product}',[ProductController::class,'update']);
-Route::delete('/deleteproduct/{product}',[ProductController::class,'destroy']);
-Route::get('/singleproductClients/{product}',[ProductController::class,'ShowSingleProductClients']);
-Route::get('/productsearch',[ProductController::class,'SearchList']);
+//Protected Stock routes
 
-//Stock routes
-
-Route::get('/stockAll',[StockController::class,'index']);
-Route::post('/createstock',[StockController::class,'store']);
-Route::put('/updatestock/{stock}',[StockController::class,'update']);
-Route::delete('/deletestock/{stock}',[StockController::class,'destroy']);
-Route::get('/singleStockList/{stock}',[StockController::class,'ShowSingleStock']);
-Route::get('/stocksearch',[StockController::class,'SearchList']);
+Route::group(['middleware' => 'auth:sanctum'],function (){
+    Route::get('/stockAll',[StockController::class,'index']);
+    Route::post('/createstock',[StockController::class,'store']);
+    Route::put('/updatestock/{stock}',[StockController::class,'update']);
+    Route::delete('/deletestock/{stock}',[StockController::class,'destroy']);
+    Route::get('/singleStockList/{stock}',[StockController::class,'ShowSingleStock']);
+    Route::get('/stocksearch',[StockController::class,'SearchList']);
+});
