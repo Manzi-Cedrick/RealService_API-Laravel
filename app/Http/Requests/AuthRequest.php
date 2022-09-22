53652683cace54957362cc05e332357592b7c0f2
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-09-22 15:34:01 +0200

    fix: Auth Request Errors

--- app/Http/Requests/AuthRequest.php
+++ app/Http/Requests/AuthRequest.php
@@ -25,35 +25,9 @@ class RegisterRequest extends FormRequest
     {
         return [
             //
-            'name'=>'required',
+            'name'=>'required|string',
             'email' =>['required','email','unique:users'],
-            'password'=> ['required','min:6']
-        ];
-    }
-}
-class LoginRequest extends FormRequest
-{
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, mixed>
-     */
-    public function rules()
-    {
-        return [
-            //
-            'email' =>['required','email','unique:users'],
-            'password'=> ['required','min:8','confirmed']
+            'password'=> ['required','min:6','confirmed']
         ];
     }
 }
