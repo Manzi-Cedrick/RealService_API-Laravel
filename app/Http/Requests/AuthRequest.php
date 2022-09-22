e22334bc03d6a3f91e121186deafdceaccdac81e
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-09-22 14:27:13 +0200

    Telephone input removal

--- app/Http/Requests/AuthRequest.php
+++ app/Http/Requests/AuthRequest.php
@@ -26,7 +26,6 @@ class RegisterRequest extends FormRequest
         return [
             //
             'name'=>'required',
-            'Telephone'=>'required',
             'email' =>['required','email','unique:users'],
             'password'=> ['required','min:6']
         ];
